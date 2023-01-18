6fc210ef4657793c8bdbeb416689e233d94b71dd
Andreastak <104064121+Andreastak86@users.noreply.github.com>
2023-01-18 11:44:04 +0100

    test changes

--- app/(personal)/layout.tsx
+++ app/(personal)/layout.tsx
@@ -3,7 +3,7 @@ import 'styles/index.css'
 import { Footer } from 'components/global/Footer'
 import { Navbar } from 'components/global/Navbar'
 import { PreviewBanner } from 'components/preview/PreviewBanner'
-import IntroTemplate from 'intro-template'
+// import IntroTemplate from 'intro-template'
 import { getSettings } from 'lib/sanity.client'
 import { getPreviewToken } from 'lib/sanity.server.preview'
 
@@ -19,12 +19,12 @@ export default async function IndexRoute({
   }
 
   return (
-    <div className="flex min-h-screen flex-col bg-white text-black">
+    <div className="flex flex-col min-h-screen text-black bg-white">
       {token && <PreviewBanner />}
       <Navbar menuItems={settings.menuItems} />
-      <div className="mt-20 flex-grow px-4 md:px-16 lg:px-32">{children}</div>
+      <div className="flex-grow px-4 mt-20 md:px-16 lg:px-32">{children}</div>
       <Footer footer={settings.footer} />
-      <IntroTemplate />
+      {/* <IntroTemplate /> */}
     </div>
   )
 }
